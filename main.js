da3481bc483aee9cdb715d01b63a8114c42ab3b7
Makoto Sato <sato@sato-mbp.local>
2012-04-21 22:04:28 +0900

    modified modal

--- main.js
+++ main.js
@@ -1,35 +1,33 @@
-// 緯度経度を設定する
-function setLatLng(lat, lng){
-var pos = new google.maps.LatLng(lat, lng);
-map.setCenter(pos);
-}
-
-var map;
-
-function initialize() {
-    var latlng = new google.maps.LatLng(35.539001,134.228468);
-    var opts = {
-	zoom: 3,
-	center: latlng,
-	mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    map = new google.maps.Map(document.getElementById("map_canvas"), opts);
-}
-
 $(function(){
-  initialize();
-  $.getJSON("jcc.txt", function(data) {
+  $.getJSON("http://craters.heroku.com/api/crater_list?sphere_id=1&offset=10&count=10", function(data) {
     $.each(data, function(i, val) {
-      var lat = this.Latitude;
+      var lat = this.latitude;
       lat = parseInt(lat.slice(0, lat.length - 1));
-      var lng = this.Longitude;
+      var lng = this.longitude;
       lng = parseInt(lng.slice(0, lng.length - 1));
-      if (this.imageurl == "no image") return;
-      var div = $("<div/>").attr("class", "item").append($("<img>").attr("class", "photo").attr("src", this.imageurl));
+      if (this.image_url == "no image") return;
+      var div = $("<div/>").attr("class", "item").append($("<img>").attr("class", "photo").attr("src", this.image_url));
       div.click(function(e) {
         e.preventDefault();
-        setLatLng(lat, lng);
+        $("#basic-modal-content").remove();
+        var basic = $("<div id='basic-modal-content'/>");
+        var map_canvas = $("<div/>")
+          .attr("id", "map_canvas")
+          .css("width", "400px")
+          .css("height", "400px");
+        var close = $("<a href='#' class='simplemodal-close'>close</a>");    
+        $(basic).append(map_canvas).append(close);
+        $(document.body).append(basic);
+        
+        var latlng = new google.maps.LatLng(lat,lng);
+        var opts = {
+          zoom: 3,
+          center: latlng,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map_canvas"), opts);
         $("#basic-modal-content").modal({persist: true, onShow: function (dialog) {
+        }, onClose: function (dialog) {
         }});
       });
 
